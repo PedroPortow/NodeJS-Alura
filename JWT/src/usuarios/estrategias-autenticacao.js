53f0d353179150c8446fc18e3cc0e2af44104116
PedroPortow <pedrolportow@gmail.com>
2022-06-14 16:07:00 -0300

    jwt

---
+++ JWT/src/usuarios/estrategias-autenticacao.js
@@ -0,0 +1,64 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy; 
+const BearerStrategy = require('passport-http-bearer').Strategy;
+
+const Usuario = require('./usuarios-modelo')
+const {InvalidArgumentError} = require('../erros');
+
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const blacklist = require('../../redis/manipula-blacklist')
+
+function verificaUsuario(usuario){
+    if(!usuario){
+        throw new InvalidArgumentError('Nao exsite usuario com esse email :(');
+    }
+}
+
+async function verificaSenha(senha, senhaHash){
+    const senhaValida = await bcrypt.compare(senha, senhaHash)
+    if(!senhaValida){
+        throw new InvalidArgumentError('Email ou senha inválidos')
+    }
+}
+
+async function verificaTokenNaBlacklist(token){
+    const tokenNaBlacklist = await blacklist.contemToken(token)
+   if(tokenNaBlacklist){
+       throw new jwt.JsonWebTokenError('Token invalido por logout')
+   }
+}
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'senha',
+        session: false
+    }, async (email, senha, done) => {
+        try{
+            const usuario = await Usuario.buscaPorEmail(email);
+            verificaUsuario(usuario);
+            await verificaSenha(senha, usuario.senhaHash)
+
+            done(null, usuario);
+        } catch(erro){
+            done(erro)
+        }
+
+    })
+)
+
+passport.use(
+    new BearerStrategy(
+        async (token, done) => {
+            try{
+                await verificaTokenNaBlacklist(token)
+                const payload = jwt.verify(token, process.env.CHAVE_JWT);
+                const usuario = await Usuario.buscaPorId(payload.id);
+                done(null, usuario, {token: token});      
+            } catch(erro){
+                done(erro)
+            }    
+        }
+    )
+)
